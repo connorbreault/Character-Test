65c17cc655a75ac8e5da06927f8bedc76a6f1646
Spaghetti Jones <connor.breault@comcast.net>
2019-09-17 14:55:05 -0700

    express boilerplate added and routed

---
+++ server.js
@@ -0,0 +1,18 @@
+// --- Dependencies --- //
+var express = require("express");
+var path = require("path");
+
+// --- Express set up --- //
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// --- Send express to routing files --- ///
+//require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+// --- Display port in console --- //
+app.listen(PORT, function () {
+    console.log("App listening on port: " + PORT);
+});
