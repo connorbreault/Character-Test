35ade9b5af21d8395ff2d07df087fc57faff9f80
Spaghetti Jones <connor.breault@comcast.net>
2019-09-17 15:52:06 -0700

    character list added

---
+++ app/data/friends.js
@@ -0,0 +1,146 @@
+var characterList = [
+    {
+        name: "Bob",
+        photo: "https://vignette.wikia.nocookie.net/bobs-burgers-fanon/images/5/51/Bob_with_a_burger.png/revision/latest?cb=20170920181424",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Linda",
+        photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Lindab.png/revision/latest?cb=20130113071354",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Tina",
+        photo: "https://cdn.imgbin.com/15/25/17/imgbin-giphy-dance-tenor-tina-belcher-others-xuwMeMC4Zjkg7jgq50mm51Y23.jpg",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Gene",
+        photo: "https://static.simpsonswiki.com/images/thumb/b/b7/Gene_Belcher.png/200px-Gene_Belcher.png",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Louise",
+        photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b9/Louise_render.png/revision/latest?cb=20130113075349",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Teddy",
+        photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/5/5e/Teddy.png/revision/latest?cb=20130305230745",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Mort",
+        photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/8/81/Mort2.png/revision/latest?cb=20130305233503",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Mr. Fischoeder",
+        photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/7/7e/Mr._Fischoeder.png/revision/latest?cb=20130305161036",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Hugo",
+        photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/1/17/Hugo_Habercore.png/revision/latest?cb=20130305170235",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+]
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,17 +1,16 @@
+// --- Dependencies --- //
 var path = require("path");
 
+// --- Export routing functions --- //
 module.exports = function (app) {
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
 
+    // --- Survey route --- //
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    // If no matching route is found default to home
-    app.get("/", function (req, res) {
+    // --- Default home route --- //
+    app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
